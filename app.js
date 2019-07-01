c2716083aa3956f3234fe7c35168abd7278521fc
Aad Versteden <madnificent@gmail.com>
2019-07-01 17:39:12 +0200

    Initializing new mu javascript service

---
+++ app.js
@@ -0,0 +1,7 @@
+# see https://github.com/mu-semtech/mu-javascript-template for more info
+
+import { app, query, errorHandler } from 'mu';
+
+app.get('/', function( req, res ) {
+  res.send('Hello mu-javascript-template');
+} );
