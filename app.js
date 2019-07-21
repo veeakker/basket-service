586b196429e4c549a20392efa17c6f4e06d0a64e
Aad Versteden <madnificent@gmail.com>
2019-07-21 13:51:17 +0200

    Initial import of the basket service
    This service is meant to create baskets and attach them to the
    session.  It might be replaced with authorization rules in the
    future.

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
-# see https://github.com/mu-semtech/mu-javascript-template for more info
+// see https://github.com/mu-semtech/mu-javascript-template for more info
 
-import { app, query, errorHandler } from 'mu';
+import { app, query, update, errorHandler, sparqlEscapeUri, sparqlEscapeString, uuid as makeUuid } from 'mu';
 
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
+app.get('/ensure', async function( req, res ) {
+  // query the database for a basket attached to the current session
+  const uuid = await ensureBasketExists(req);
+
+  res.send(JSON.stringify({
+    data: {
+      type: "baskets",
+      id: uuid
+    }
+  }));
 } );
+
+async function ensureBasketExists(req) {
+  const sessionId = req.get('mu-session-id');
+
+  const response = await query(`PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+
+    SELECT ?uuid WHERE {
+      GRAPH <http://mu.semte.ch/application> {
+        ${sparqlEscapeUri(sessionId)} veeakker:hasBasket/mu:uuid ?uuid
+      }
+    }`);
+
+  if( response.results.bindings.length > 0 ) {
+    return response.results.bindings[0].uuid.value;
+  } else {
+    const uuid = await makeBasket( req );
+    return uuid;
+  }
+}
+
+async function makeBasket( req ) {
+  const sessionId = req.get('mu-session-id');
+  let uuid = makeUuid();
+
+  await update(`PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+
+    INSERT DATA {
+      GRAPH <http://mu.semte.ch/application> {
+        ${sparqlEscapeUri(sessionId)} veeakker:hasBasket <http://veeakker.be/baskets/${uuid}>.
+        <http://veeakker.be/baskets/${uuid}>
+          a veeakker:Basket;
+          mu:uuid ${sparqlEscapeString( uuid )}.
+      }
+    }`);
+
+  return uuid;
+}
+
+app.use(errorHandler);
