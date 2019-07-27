c286a0aaa77c270d6007041117298604846eb58a
Aad Versteden <madnificent@gmail.com>
2019-07-27 20:32:22 +0200

    Make basket service aware of basket draft status
    Baskets which haven't been ordered should be in the draft status.
    This is now happening with the basket service.  The service creates
    the baskets in a draft status and only returns draft baskets when the
    current basket is requested (a completed basket should no longer be
    edited by the user).
    
    We suspect other services may move these non-draft baskets around in
    order to ensure that they cannot be changed anymore.  That is not the
    goal of the current service se we ignore that situation for now.  The
    final implementation may change to cope with that.

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 // see https://github.com/mu-semtech/mu-javascript-template for more info
 
-import { app, query, update, errorHandler, sparqlEscapeUri, sparqlEscapeString, uuid as makeUuid } from 'mu';
+import { app, query, update, errorHandler, sparqlEscapeUri, sparqlEscapeString, sparqlEscapeDateTime, uuid as makeUuid } from 'mu';
 
-app.get('/ensure', async function( req, res ) {
+app.get('/ensure', async function( req, res, next ) {
   // query the database for a basket attached to the current session
-  const uuid = await ensureBasketExists(req);
+  try {
+    const uuid = await ensureBasketExists(req);
 
-  res.send(JSON.stringify({
-    data: {
-      type: "baskets",
-      id: uuid
-    }
-  }));
+    res.send(JSON.stringify({
+      data: {
+        type: "baskets",
+        id: uuid
+      }
+    }));
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
 } );
 
 async function ensureBasketExists(req) {
   const sessionId = req.get('mu-session-id');
 
-  const response = await query(`PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
+  const basketQuery = `
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
 
     SELECT ?uuid WHERE {
       GRAPH <http://mu.semte.ch/application> {
-        ${sparqlEscapeUri(sessionId)} veeakker:hasBasket/mu:uuid ?uuid
+        ${sparqlEscapeUri(sessionId)} veeakker:hasBasket ?basket.
+        ?basket veeakker:basketOrderStatus <http://veeakker.be/order-statuses/draft>;
+          mu:uuid ?uuid.
       }
-    }`);
+    }`;
 
+  const response = await query(basketQuery);
   if( response.results.bindings.length > 0 ) {
     return response.results.bindings[0].uuid.value;
   } else {
@@ -38,15 +47,18 @@ async function makeBasket( req ) {
   const sessionId = req.get('mu-session-id');
   let uuid = makeUuid();
 
-  await update(`PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
+  await update(`
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX veeakker: <http://veeakker.be/vocabularies/shop/>
 
     INSERT DATA {
       GRAPH <http://mu.semte.ch/application> {
         ${sparqlEscapeUri(sessionId)} veeakker:hasBasket <http://veeakker.be/baskets/${uuid}>.
         <http://veeakker.be/baskets/${uuid}>
           a veeakker:Basket;
-          mu:uuid ${sparqlEscapeString( uuid )}.
+          mu:uuid ${sparqlEscapeString( uuid )};
+          veeakker:basketOrderStatus <http://veeakker.be/order-statuses/draft>;
+          veeakker:statusChangedAt ${sparqlEscapeDateTime(new Date())}.
       }
     }`);
 
